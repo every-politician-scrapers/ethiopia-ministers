59259766c75ce3e2e444c0f80fca2f606695da51
Tony Bowden <tony@tmtm.com>
2022-04-28 15:16:04 +0300

    less verbose comparisons

--- bin/compare-wikipedia.sh
+++ bin/compare-wikipedia.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-bundle exec ruby bin/scraper/wikipedia.rb | ifne tee data/wikipedia.csv
+bundle exec ruby bin/scraper/wikipedia.rb > data/wikipedia.csv
 bundle exec ruby bin/wikipedia-diff.rb | tee data/wikipedia-diff.csv
